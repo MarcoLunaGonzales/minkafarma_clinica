a90b0e0aa24f1d061c2e45f26ca0a1f72cf4ba69
Marco A. Luna G. <lunagonzalesmarco@gmail.com>
2025-11-07 07:36:21 -0400

    cambios

--- listaRazonActual.php
+++ listaRazonActual.php
@@ -3,15 +3,22 @@ $estilosVenta=1;
 require("conexionmysqli2.inc");
 $rs=$_GET['rs'];
 
-$sql="select DISTINCT f.nit,f.razon_social from facturas_venta f 
-	where f.razon_social like '%$rs%' and f.nit!='' and f.razon_social!='' order by f.fecha desc limit 0,5";
+$sql="select DISTINCT s.nit, s.razon_social from salida_almacenes s 
+	where s.razon_social like '%$rs%' and s.nit!='' and s.razon_social!='' order by s.fecha desc limit 0,5";
 
+//	echo $sql;
 $resp=mysqli_query($enlaceCon,$sql);
 $index=0;
 while($dat=mysqli_fetch_array($resp)){
 	$rsocial=$dat[1];
 	$nit=$dat[0];
-	?><tr><td><?=$rsocial?></td><td><?=$nit?></td><td><a href="#" onclick="asignarNit('<?=$nit?>');return false;" class="btn btn-rose btn-sm btn-fab"><i class="material-icons">check</i></a></td></tr><?php
+	?>
+<tr>
+		<td><?=$rsocial?></td>
+		<td><?=$nit?></td>
+		<td><a href="#" onclick="asignarNit('<?=$nit?>');return false;" class="btn btn-rose btn-sm btn-fab"><i class="material-icons">check</i></a></td>
+	</tr>
+<?php
 	$index++;
 }
 
